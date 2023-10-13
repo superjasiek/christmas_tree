9ce5aab3805bac6a7d7ec3c0effe70b65ed0f7bf
superjasiek <liniany@poczta.fm>
2023-10-13 17:27:05 +0200

    Add files via upload

---
+++ tree_2023.ino
@@ -0,0 +1,63 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 9600000
+#define LED PB0 
+
+void mode_0(void);
+void mode_1(void);
+void mode_2(void);
+void mode_3(void);
+
+int main(void)
+{
+    DDRB = _BV(PB0) | _BV(PB1) | _BV(PB2) | _BV(PB3) | _BV(PB4);
+
+    while (1) {
+        mode_1();
+        mode_0();
+        mode_1();
+        mode_0();
+        mode_3();
+        mode_0();
+
+
+    }
+
+    return 0;
+}
+
+void mode_0(void)
+{
+    for (int i = 0; i < 5; i++) {
+        PORTB = _BV(i);
+        _delay_ms(1000);
+    }
+
+    PORTB = 0;
+}
+
+void mode_1(void)
+{
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            PORTB |= _BV(j);
+            _delay_ms(100);
+        }
+
+        for (int j = 0; j < 5; j++) {
+            PORTB &= ~(_BV(j));
+            _delay_ms(100);
+        }
+    }
+}
+void mode_3(void)
+{
+    for (int i = 0; i < 1; i++) {
+        _delay_ms(1000);
+        PORTB = _BV(PB0) | _BV(PB1) | _BV(PB2) | _BV(PB3) | _BV(PB4);
+        _delay_ms(3000);
+        PORTB = 0;
+    }
+
+    _delay_ms(1000);
+}
